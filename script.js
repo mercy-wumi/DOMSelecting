51660142456cb685fa80ed3a46fc4a3c8e53c24a
mercy-wumi <m.adams1909@gmail.com>
2022-01-07 00:33:53 +0100

    dom manipulation

---
+++ script.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('#History')
+header.style.color = 'teal'
+let allImages = document.querySelectorAll('img')
+for (img of allImages) {
+    img.style.borderRadius = '10px'
+}
